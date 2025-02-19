f525edeeb4c92cdaa2fc2c4552021b915b65e49b
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-19 07:42:45 +0530

    bellmon ford algo

--- src/main/java/org/example/practice/Classroom.java
+++ src/main/java/org/example/practice/Classroom.java
@@ -275,6 +275,52 @@ public class Classroom {
 
     }
 
+    public static void BellmonFord(ArrayList<Edge> graph[], int src, int V)
+    {
+        int[] dist = new int[V];
+        for(int i=0; i<V; i++)
+        {
+            if(i!=src)
+            {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        for(int k=0; k<V-1; k++)
+        {
+            for(int i=0; i<V; i++)
+            {
+                for(int j=0; j<graph[i].size(); j++)
+                {
+                    Edge e = graph[i].get(j);
+                    int u = e.src;
+                    int v = e.dest;
+
+                    if(dist[u] != Integer.MAX_VALUE && dist[u] + e.wt < dist[v])
+                    {
+                        dist[v] = dist[u] + e.wt;
+                    }
+                }
+            }
+        }
+        
+        //-ve weight cycle
+        for(int i=0; i<V; i++)
+        {
+            for(int j=0; j<graph[i].size(); j++)
+            {
+                Edge e = graph[i].get(j);
+                int u = e.src;
+                int v = e.dest;
+
+                if(dist[u] != Integer.MAX_VALUE && dist[u] + e.wt < dist[v])
+                {
+                    System.out.println("graph has -ve weight cycle");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
